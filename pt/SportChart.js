511c2e265c7dc5d9e0d5940cfb2bf45ea2a70cc1
User <user@Users-MacBook-Air.local>
2017-03-16 10:37:37 +1300

    sportchart ios

--- pt/SportChart.js
+++ pt/SportChart.js
@@ -10,7 +10,8 @@ import {
   Animated,
   Picker,
   ScrollView,
-  AsyncStorage
+  AsyncStorage,
+  TouchableOpacity
 } from 'react-native';
 
 import { Bar } from 'react-native-pathjs-charts';
@@ -18,12 +19,10 @@ import { Bar } from 'react-native-pathjs-charts';
 
 var _navigator ;
 
-class SportChartView extends React.Component {
+var SportChartView = React.createClass({
 
-  constructor() {
-    super();
-      
 
+   getInitialState: function(){
     this.state = {
       sport:'',
       data:[
@@ -43,8 +42,16 @@ class SportChartView extends React.Component {
     sportselected:'',
       sportname:['BB BENCH PRESS', 'DB FLYS', 'INCLINE DB BENCH','Rower','Treadmill']
   };
+  return {
+    sportselected:this.state.sportselected,
+    sportname:this.state.sportname,
+    legend:this.state.legend,
+    data:this.state.data,
+    type:this.state.type
+    };
 
-}
+
+  },
 
 
   //set the item and data
@@ -125,8 +132,85 @@ componentWillMount() {
         });
       });
 
-  }  
-  render() {
+  },
+    //UPDATE the CHART 
+ UPDATE:function() {
+    let _that=this; 
+    function format (d) {
+            return d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
+        }
+    var today =new Date();
+    var end = format(today);
+    var day1=new Date(today.getTime() - (1000* 60 * 60 * 24)*6);
+    var startday=format(day1);
+    var itemname='Burpees';
+    console.log(this.state.sportselected);
+    var itemname=this.state.sportselected;
+    var item_id;
+     AsyncStorage.getItem('userid',(err, result) => {
+     console.log(result);
+      var trainee_id=result;
+      var url = 'http://47.90.60.206:8080/pt_server/item.action'; // get the item data again 
+      fetch(url).then(function(response) {  
+                return response.json();
+              }).then(function(res) {
+               if (res["data"]!=null) {
+               //find the id of selected item
+               
+               for(i in res["data"]){
+                if(itemname==res["data"][i]["name"]){
+                   item_id=res["data"][i]["id"];
+                }
+                 
+               }
+                console.log(item_id);
+               var urlupdate = 'http://47.90.60.206:8080/pt_server/statsport.action';
+        
+               urlupdate += '?trainee_id='+trainee_id+'&start='+startday+'&end='+end+'&item_id='+item_id;
+                console.log(urlupdate);
+
+                   fetch(urlupdate).then(function(response) {  
+                                return response.json();
+                              }).then(function(res) {
+                              console.log(res);
+                              if (res["data"]!=null) {
+                                var arr=[]
+                                for (var i = 0; i < res["data"].length; i++) {
+                                  var obj={}
+                                  obj.v =res["data"][i]["energy"];
+                                  obj.name=res["data"][i]["day"];
+                                  arr.push(obj)
+                                };
+                                console.log(arr);
+                                
+                                var chartdata=[];
+                                for (var i = 0; i < arr.length; i++) {
+                                  var arrt=[];
+                                  arrt.push(arr[i]);
+                                  chartdata.push(arrt);
+                                  
+                                };
+                                _that.setState({
+                                  data:chartdata
+                                })
+                  
+                              }else{
+                                  Alert.alert('Fail to display','Please check your data'); 
+                               }
+                });
+                    
+              }else{
+                Alert.alert('Fail to display','Please check your data'); 
+          }
+          
+       
+       });
+
+
+    });
+
+ },
+  render: function(){
     
     let data=this.state.data;
 
@@ -178,7 +262,7 @@ componentWillMount() {
 
     return (
         <View style={styles.container}>
-
+  
         <Bar data={data} options={options} accessorKey='v'/>
         <Picker
           prompt="Please choose sportclass"
@@ -192,21 +276,42 @@ componentWillMount() {
                                  label={s} />
                      }) }
         </Picker>
+       <View>
+              <TouchableOpacity style={styles.btn}
+              onPress={this.UPDATE}>
+              <Text style={styles.text}>UPDATE</Text>
+              </TouchableOpacity>
+       </View> 
       </View>
  
     )
   }
-}
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#F5FCFF',
+         alignItems: 'center',
+
+
   },
     sportact:{
-      flex:1,
+    flexGrow:1,
     height:50,
     width:200,
+  },
+  btn:{
+     alignItems: 'center',
+     justifyContent: 'center',
+     backgroundColor: '#2cb395',
+     height: 30,
+     borderRadius: 5,
+     width:400,
+  },
+    text:{
+    fontSize:18,
+    color:'#fff',
   },
    
 });
