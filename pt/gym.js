379a02d7bbdee9e0d754d7a253dd2a757069317d
User <user@Users-MacBook-Air.local>
2017-03-23 16:37:55 +1300

    fake gym and trainer

--- pt/gym.js
+++ pt/gym.js
@@ -18,121 +18,31 @@ import {
 
 import { Icon } from 'react-native-elements';
 import Dimensions from 'Dimensions';
-import Swipeout from 'react-native-swipeout';
 
-var screenW = Dimensions.get('window').width;
+import { SearchBar } from 'react-native-elements'
+import Swiper from 'react-native-swiper';
+
+var width = Dimensions.get('window').width;
 
 
 var _navigator ;
 
-var btnsDefault = [ { text: 'Button' } ];
-
-  var btnsTypes = [
-      { text: 'Edit', onPress: function(){ _navigator.push({
-                title:'EditRecordView',
-                id:'editrecord'
-              })},type: 'primary',},
-        { text: 'Delete',onPress: function(){ alert('Confirm to delete?') },type: 'delete'},
-  ];
-
-var rows = [
-  {
-     Pdate:"11-01-2017",
-     Calories :"457",
-     text: "Row:5min;Treadmill:6min;Xtrainer:5min",
-
-    right:btnsTypes,
-    autoClose: true,
-  }, {
-    Pdate:"14-01-2017",
-    Calories :"457",
-     text: "Row:5min;Treadmill:6min;Xtrainer:5min",
-     right:btnsTypes,
-    autoClose: true,
-  }, {
-      Pdate:"19-01-2017",
-      Calories :"457",
-      text: "Row:5min;Treadmill:6min;Xtrainer:5min",
-     right:btnsTypes,
-    autoClose: true,
-  }, {
-    Pdate:"21-01-2017",
-    Calories :"457",
-    text: "Row:5min;Treadmill:6min;Xtrainer:5min",
-     right:btnsTypes,
-  },
-  
-];
 
 var GymView = React.createClass({
 
   getInitialState: function(){
     _navigator = this.props.navigator;
-    var ds = new ListView.DataSource({rowHasChanged: (row1, row2) => true});
-    var Pdate="Monday";
-    var rowIDs = [];
+    
     this.state = {
-      dataSource: ds.cloneWithRows(rows),
-      scrollEnabled: true,
 
     };
     return {
-      dataSource: this.state.dataSource,
-      scrollEnabled: true,
+
 
     };
 
   },
 
-//  set scrolling to true/false
-  allowScroll(scrollEnabled) {
-    this.setState({ scrollEnabled: scrollEnabled });
-  },
-
-  //  set active swipeout item
-   handleSwipeout(sectionID,rowID) {
-    for (var i = 0; i < rows.length; i++) {
-
-      if (i != rowID) rows[i].active = false;
-      else rows[i].active = true;
-    }
-    this.updateDataSource(rows);
-  },
-
-  updateDataSource(data) {
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(data),
-    });
-  },
-
-
-  renderRow(rowData: string, sectionID: number, rowID: number) {
-     var btnsTypes = [
-        { text: 'Delete',onPress: () => { this.delete(rowData) },type: 'delete'},
-      ];
-    return (
-      <Swipeout
-        left={rowData.left}
-        right={btnsTypes}
-        rowID={rowID}
-        sectionID={sectionID}
-        autoClose={rowData.autoClose}
-        backgroundColor={rowData.backgroundColor}
-        close={!rowData.active}
-        onOpen={(sectionID, rowID) => this.handleSwipeout(sectionID, rowID) }
-        scroll={event => this.allowScroll(event)}>
-        <TouchableOpacity style={styles.btn}
-                onPress={() => _navigator.push({title:'DetailRecordView',id:'detailrecord',params:{date:rowData.day}})}>
-          <View style={styles.li}>
-            <View  style={styles.lidate}><Image  source={require('../img/plan_normal.png') }/><Text>{rowData.day}</Text></View>
-            
-              <Text style={styles.liText}>Sports: {rowData.text}</Text>
-            
-          </View>
-        </TouchableOpacity>
-      </Swipeout>
-    );
-  },
 
 
  render: function(){
@@ -159,14 +69,25 @@ var GymView = React.createClass({
               </View>
               
             </View>
+          <SearchBar
+             
+              placeholder='Find your Gym here' />             
+        <Swiper style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Join a gym</Text>
+          <Image resizeMode='stretch' style={styles.image} source={require('../img/gym1.jpg')} />
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        <Image resizeMode='stretch' style={styles.image} source={require('../img/gym1.jpg')} />
 
-            <ListView style={styles.listview}
-              scrollEnabled={this.state.scrollEnabled}
-              dataSource={this.state.dataSource}
-              renderRow={this.renderRow}
-              enableEmptySections={true}
-              />
-                  
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        <Image resizeMode='stretch' style={styles.image} source={require('../img/gym1.jpg')} />
+
+        </View>
+      </Swiper>       
 
            
         </ScrollView>
@@ -215,30 +136,31 @@ var styles = StyleSheet.create({
     flexDirection:'column',
 
   },
-  listview: {
+    wrapper: {
+  },
+  slide1: {
     flex: 1,
-  },
-  li: {
-    backgroundColor: '#fff',
-    borderBottomColor: '#38bda0',
-    borderColor: 'transparent',
-    borderWidth: 1,
-    paddingLeft: 16,
-    paddingTop: 14,
-    height:120,
-    paddingBottom: 16,
-  },
-  liContainer: {
-    flex: 2,
-  },
-  liText: {
-    color: '#333',
-    fontSize: 18,
-  },
-  lidate:{
-    flex: 1,
-    flexDirection:'row',
+    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#70f0d4',
   },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#db84d4',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4368db',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+ 
 });
 module.exports = GymView;
