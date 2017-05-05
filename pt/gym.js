c6072017ef8d7a8ff3cd1e745ed0e5d48d9a50a8
User <user@Users-Air.station>
2017-05-05 22:02:06 +1200

    gmap init

--- pt/gym.js
+++ pt/gym.js
@@ -14,60 +14,63 @@ import {
 } from 'react-native';
 import { Icon } from 'react-native-elements';
 import Dimensions from 'Dimensions';
+import ScrollableTabView , { ScrollableTabBar, }from'react-native-scrollable-tab-view';
 import { SearchBar } from 'react-native-elements'
 import Swiper from 'react-native-swiper';
+import GymmapView from './gymmap';
+import GymlistView from './gymlist';
 import URLnetowrk from './network';
 var width = Dimensions.get('window').width;
 var _navigator ;
 var GymView = React.createClass({
   getInitialState: function(){
-    _navigator = this.props.navigator; 
+    _navigator = this.props.navigator;  
     this.state = {
     };
     return {
     };
   },
- render: function(){
-  return(
-     <ScrollView 
-        contentContainerStyle={{flex:1}}
-        keyboardDismissMode='on-drag'
-        keyboardShouldPersistTaps="always">
+  render: function(){
+    return(
+      <ScrollView 
+          contentContainerStyle={{flex:1}}
+          keyboardDismissMode='on-drag'
+          keyboardShouldPersistTaps="never">
         <View style={[styles.Top,styles.Bottomline]}>
-            <TouchableOpacity 
-            onPress={() => _navigator.push({title:'Additemtoday',id:'additemtoday'})}>
-              <Image source={require('../img/add_pressed.png') }/>
-            </TouchableOpacity> 
+          <TouchableOpacity 
+              onPress={() => _navigator.push({title:'Additemtoday',id:'additemtoday'})}>
+            <Image source={require('../img/add_pressed.png') }/>
+          </TouchableOpacity> 
           <View style={styles.Topbar}>
           </View>
           <View style={styles.right}>
-          <TouchableOpacity 
-                  onPress={() => _navigator.push({title:'ChartView',id:'chart'})}>
-            <Image source={require('../img/chart-pressed.png') }/>
-          </TouchableOpacity> 
-          </View>         
+            <TouchableOpacity 
+                      onPress={() => _navigator.push({title:'ChartView',id:'chart'})}>
+              <Image source={require('../img/chart-pressed.png') }/>
+            </TouchableOpacity> 
+          </View>
         </View>
-      <SearchBar         
-          placeholder='Find your Gym here' />             
-      <Swiper style={styles.wrapper} showsButtons={true}>
-        <View style={styles.slide1}>
-          <Text style={styles.text}>Join a gym</Text>
-          <Image resizeMode='stretch' style={styles.image} source={require('../img/gym1.jpg')} />
-        </View>
-        <View style={styles.slide2}>
-          <Text style={styles.text}>Beautiful</Text>
-        <Image resizeMode='stretch' style={styles.image} source={require('../img/gym2.jpg')} />
-        </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>And simple</Text>
-        <Image resizeMode='stretch' style={styles.image} source={require('../img/gym3.jpg')} />
-       </View>
-      </Swiper>              
-    </ScrollView>
+        <SearchBar
+            placeholder='Find your Gym here' />             
+        <ScrollableTabView           
+            initialPage={1}
+            renderTabBar={() => <ScrollableTabBar  />}
+          >
+            <ScrollView tabLabel="Map" style={styles.tabView}>
+              <View style={styles.card}>
+                <GymmapView {...this.props}/>
+              </View>
+            </ScrollView>
+            <ScrollView tabLabel="List" style={styles.tabView}>
+              <View style={styles.card}>
+                <GymlistView {...this.props}/>
+              </View>
+            </ScrollView>
+          </ScrollableTabView>    
+      </ScrollView>
     );
   },
 });
-
 var styles = StyleSheet.create({
    container:{
     flex: 1,
@@ -85,9 +88,11 @@ var styles = StyleSheet.create({
     borderBottomWidth:2,
     borderColor:'gray'
   },
+
   Topbar:{
     flex:2,
     flexDirection: 'row',
+
   },
    Left:{
     flex:1,
@@ -96,39 +101,35 @@ var styles = StyleSheet.create({
   Right:{
   flex:1,
   flexDirection: 'row',
+
   },
   maincontain:
   {
     flex: 1,
     backgroundColor: '#38bda0',
     flexDirection:'column',
-
-  },
-    wrapper: {
-  },
-  slide1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#70f0d4',
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#db84d4',
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4368db',
   },
   text: {
     color: '#fff',
     fontSize: 30,
     fontWeight: 'bold',
-  }
- 
+  },
+  tabView: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#38bda0',
+  },
+  card: {
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    borderColor: 'rgba(0,0,0,0.1)',
+    margin: 5,
+    height:400,
+    padding: 15,
+    shadowColor: '#ccc',
+    shadowOffset: { width: 2, height: 2, },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+  },
 });
 module.exports = GymView;
