58d99098a751e080e36bf1ffc13a0021a9a61e9b
User <user@Users-MacBook-Air.local>
2017-05-22 10:53:27 +1200

    creategym api

--- pub/gymcreate.js
+++ pub/gymcreate.js
@@ -28,7 +28,8 @@ var Gym = t.struct({
   slogan:t.String,
   open:t.String,
   contact:t.Number,
-  location:t.String
+  location:t.String,
+  description:t.String
   //rememberMe: t.Boolean        // a boolean
 });
 BackAndroid.addEventListener('hardwareBackPress', function() {
@@ -49,6 +50,32 @@ var Gymcreate = React.createClass({
     };
     return {
     };
+  },
+ _save:function(){
+  var value = this.refs.form.getValue();
+  var name = value["name"];
+  var opendate=value["open"];
+  var slogan = value["slogan"];
+  var location = value["location"];
+  var contact =value["contact"];
+  var description=value["description"];
+  var url=URLnetowrk+'create_gym.action'; 
+  url += '?name='+name+'&location='+location+'&opendate='+opendate+'&description='+description+'&contact='+contact+'&slogan='+slogan;
+  console.log(url);
+  fetch(url).then(function(response) {  
+    return response.json();
+  }).then(function(res) {
+    if (res["data"]!=null) {
+    console.log(res);
+      _navigator.push({
+      title:'ThomeView',
+      id:'Thome',
+    })
+    }
+    else{
+       Alert.alert('Fail to display','Please check your data'); 
+    }
+  })          
   },
   render: function(){
     return(
